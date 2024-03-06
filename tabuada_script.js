176c9ab62d45fa36d33290ae80e87501273ee424
Eder Tormen da Silveira <steder20@yahoo.com.br>
2024-03-05 23:27:09 -0300

    Created JS file

---
+++ tabuada_script.js
@@ -0,0 +1,21 @@
+/*let number
+number = window.document.querySelector('#inputs#num')
+
+let endpoint
+endpoint = window.document.querySelector('#inputs#end')
+
+const answers = ['endpoint']
+
+function Tabuada(number, endpoint){
+    for (let index = 0; index <= endpoint; index++) {
+        answers[index] = number * index;
+    }
+
+    alert(answers);
+}*/
+
+function Tabuada(){
+    
+
+    alert('answers');
+}
