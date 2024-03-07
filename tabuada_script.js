c61c4ad7d166beaefa0591f793197c053384a3fb
Eder Tormen da Silveira <steder20@yahoo.com.br>
2024-03-07 00:32:51 -0300

    Added alert mensage

--- tabuada_script.js
+++ tabuada_script.js
@@ -5,16 +5,22 @@ function Tabuada(){
     var end = document.getElementById('end')
     var endpoint = Number(end.value)
 
-    const answers = [endpoint]
+    if (num.value.length == 0 || end.value.length == 0) {
+        alert('Preencha os campos com algum valor!')
+    } else {
+        const answers = [endpoint]
 
-    var answer = document.getElementById('answers')
+        var answer = document.getElementById('answers')
 
-    answer.innerHTML = '<p>A tabuada do ' + number + ' é:</p>'
+        answer.innerHTML = '<p>A tabuada do ' + number + ' é:</p>'
 
-    for (let index = 0; index <= endpoint; index++) {
-        answers[index] = number * index
+        for (let index = 0; index <= endpoint; index++) {
+            answers[index] = number * index
 
-        answer.innerHTML += number + ' X ' + index + ' = ' + answers[index] + '<br>'
+            answer.innerHTML += number + ' X ' + index + ' = ' + answers[index] + '<br>'
+        }
     }
+
+    
     
 }
